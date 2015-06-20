0c11706690ff7b534b3de5a7417db2e0465a08db
JParis44 <j.paris.caldwell@gmail.com>
2015-06-20 15:30:00 -0500

    Continued work on UI

--- watchout.js
+++ watchout.js
@@ -8,8 +8,9 @@ var gameValues = {
   maxCollisions : 5,
   asteroids : [],
   numberOfAsteroids : 20,
-  asteroidRadius: 10,
-  playerRadius: 10
+  asteroidRadius: 15,
+  playerRadius: 10,
+  running: true // Is game running?
 }
 
 //Initialize the board
@@ -29,8 +30,6 @@ board.append('circle').attr('id', 'playerOne')
 //Setup the initial board with asteroids and scoreboard
 var setup = function(){
 
-  // Stop any previous game
-  clearTimeout(update);
 
   //Empty the lisst of asteroid objects
   gameValues.asteroids.length = 0;
@@ -57,8 +56,6 @@ var setup = function(){
 
   //Add the player to the DOM board
   board.select('#playerOne')
-    .attr('cx', gameValues.boardWidth/2)
-    .attr('cy', gameValues.boardHeight/2)
     .attr('r', gameValues.playerRadius);
 
 
@@ -66,48 +63,47 @@ var setup = function(){
   gameValues.currentScore = 0;
   gameValues.collisions = 0;
 
-  // Start game
-  setTimeout(update, 5000);
+  gameValues.running = true;
 
 };
 
 var update = function(){
 
+  if(gameValues.running) {
+
+    for (var i = 0; i < gameValues.numberOfAsteroids; i++) {
+      gameValues.asteroids[i].top = Math.random() * board.attr('height');
+      gameValues.asteroids[i].left = Math.random() * board.attr('width');
+    }
+
+    d3.selectAll('.asteroids').data(gameValues.asteroids).transition()
+      .duration(900)
+      .tween('d', function(asteroid){
+        var startX = d3.select(this).attr('cx');
+        var startY = d3.select(this).attr('cy');
+        var current = d3.select(this);
+        var interpolatorX = d3.interpolateNumber( startX, asteroid.left);
+        var interpolatorY = d3.interpolateNumber( startY, asteroid.top);
 
 
-  for (var i = 0; i < gameValues.numberOfAsteroids; i++) {
-    gameValues.asteroids[i].top = Math.random() * board.attr('height');
-    gameValues.asteroids[i].left = Math.random() * board.attr('width');
+        return function(t){
+          var x = interpolatorX(t);
+          var y = interpolatorY(t);
+          collisionDetection(x, y, current);
+          d3.select(this).attr('cx', x).attr('cy', y);
+        };
+
+      });
+
+    gameValues.currentScore++;
+
+    if(gameValues.currentScore > gameValues.highScore) { gameValues.highScore++; }
+
+    d3.select('#highVal').text(gameValues.highScore);
+    d3.select('#currentVal').text(gameValues.currentScore);
+
+
   }
-
-  d3.selectAll('.asteroids').data(gameValues.asteroids).transition()
-    .duration(900)
-    .tween('d', function(asteroid){
-      var startX = d3.select(this).attr('cx');
-      var startY = d3.select(this).attr('cy');
-      var current = d3.select(this);
-      var interpolatorX = d3.interpolateNumber( startX, asteroid.left);
-      var interpolatorY = d3.interpolateNumber( startY, asteroid.top);
-
-
-      return function(t){
-        var x = interpolatorX(t);
-        var y = interpolatorY(t);
-        collisionDetection(x, y, current);
-        d3.select(this).attr('cx', x).attr('cy', y);
-      };
-
-    });
-
-  gameValues.currentScore++;
-
-  if(gameValues.currentScore > gameValues.highScore) { gameValues.highScore++; }
-
-  d3.select('#highVal').text(gameValues.highScore);
-  d3.select('#currentVal').text(gameValues.currentScore);
-
-  // Schedule next update
-  setTimeout(update, 1000);
 
 };
 
@@ -121,8 +117,10 @@ var collisionDetection = function(x, y, current){
       gameValues.collisions++;
       d3.select('#collisionsVal').text(gameValues.collisions);
       current.attr('collided', 'true');
-      if (gameValues.collisions >= gameValues.maxCollisions){
+      if (gameValues.collisions >= gameValues.maxCollisions && gameValues.running){
+        gameValues.running = false;
         setup();
+
       }
 
    //   d3.select("#failMessage").style("display:")
@@ -148,6 +146,7 @@ d3.select('.board').on('mousemove', function(){
 });
 
 setup();
+setInterval(update, 1000)
 /*
 
 d3.selectAll('.asteroids').on('mouseenter', function(){
